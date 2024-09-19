e335f9d2438de013b59a9d9e97a6844773f27614
May Thagyan <may.thagyan@privasia.com>
2024-09-19 18:02:05 +0800

    init

--- src/com/may/oca/Animals.java
+++ src/com/may/oca/Animals.java
@@ -1,5 +1,23 @@
 package com.may.oca;
 
 public class Animals {
+	String name;
+	String species;
+
+	public void sound() {
+		System.out.println("This animal is a dog and it barks");
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
 
 }
---
+++ src/com/may/oca/Zoo.java
@@ -0,0 +1,9 @@
+package com.may.oca;
+
+public class Zoo {
+	
+	public static void main(String[] args) {
+		System.out.println("Main Method");
+		System.out.println("Args[0] " + args[0]);
+	}
+}
